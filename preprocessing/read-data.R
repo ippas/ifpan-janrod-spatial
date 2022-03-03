9eb5e1e9dc80691a702bcf29ee30302814246a62
mateuszzieba <mateuszzieba97@gmail.com>
2022-03-03 12:34:49 +0000

    update scripts R

---
+++ preprocessing/read-data.R
@@ -0,0 +1,89 @@
+#############################################################
+## Stuff I have to do
+## mateuszzieba97@gmail.com - Feb 2022
+#############################################################
+
+meta_data <- read.table("data/samples-spatial-metadata.tsv",
+                        header = TRUE,
+                        sep = "\t")
+
+# Require source files
+info_peaks <- read.table("data/gene-annotation/peaks-annotate-sort.bed", 
+                         header = FALSE,
+                         sep = "\t",
+                         col.names = c('chr_peak',
+                                       'start_peak',
+                                       'end_peak',
+                                       'peak_id',
+                                       'score_int(-10*log10pvalue)',
+                                       'strand_coverage',
+                                       'fold_change_peak_summit',
+                                       '-log10pvalue_peak_summit',
+                                       '-log10qvalue_peak_summit',
+                                       'relative_summit_position_peak_start',
+                                       'type_peak',
+                                       'chr_gene',
+                                       'start_gene',
+                                       'end_gene',
+                                       'gene_id',
+                                       'gene_name',
+                                       'strand_gene')) 
+
+
+###############
+# read images #
+###############
+lapply(samples_name, 
+       function(x) paste("data/spaceranger-corrected/", 
+                         x, 
+                         "/outs/spatial/tissue_lowres_image.png",
+                         sep = "") %>%
+         read.bitmap(.)) -> images_cl
+
+
+###############################
+# Convert the Images to grobs #
+###############################
+lapply(images_cl, 
+       function(x) rasterGrob(x, 
+                              width = unit(1, "npc"),
+                              height = unit(1, "npc"))) %>%
+  tibble(sample=factor(samples_name), grob =.) %>%
+  mutate(height = (rapply(images_cl, 
+                          function(x) data.frame(height = nrow(x)))),
+         width = (rapply(images_cl,
+                         function(x) data.frame(weight = ncol(x))))) -> images_tibble
+
+
+# create table bcs_merge containing information about barcode spots
+# for all samples
+lapply(samples_name,
+       # load data about position tissue
+       function(x, y = x) read.csv(
+         paste("data/spaceranger-corrected/", 
+               x, 
+               "/outs/spatial/tissue_positions_list.csv",
+               sep = ""),
+         col.names=c("barcode","tissue","row","col","imagerow","imagecol"), 
+         header = FALSE) %>% 
+         
+         mutate(
+           imagerow = imagerow * (paste("data/spaceranger-corrected/", 
+                                        x, 
+                                        "/outs/spatial/scalefactors_json.json",
+                                        sep = "") %>%
+                                    rjson::fromJSON(file = .) %>% 
+                                    .$tissue_lowres_scalef),
+           imagecol = imagecol * (paste("data/spaceranger-corrected/", 
+                                        x, 
+                                        "/outs/spatial/scalefactors_json.json",
+                                        sep = "") %>%
+                                    rjson::fromJSON(file = .) %>% 
+                                    .$tissue_lowres_scalef),
+           tissue = as.factor(tissue),
+           height = images_tibble[images_tibble$sample == x,]$height,
+           width = images_tibble[images_tibble$sample == x,]$width
+         )
+) %>% 
+  setNames(samples_name) %>% 
+  bind_rows(., .id = "sample") -> bcs_merge
--- preprocessing/visualization-seurat.R
+++ preprocessing/visualization-seurat.R
@@ -2,10 +2,7 @@
 ## Stuff I have to do
 ## mateuszzieba97@gmail.com - Feb 2022
 #############################################################
-install.packages("roxygen2")
-require(roxygen2)
-install.packages("devtools")
-require(devtools)
+
 
 integrated_analysis <- FindClusters(integrated_analysis, resolution = 0.5)
 
@@ -37,6 +34,10 @@ for(sample_name in samples_name) {
      new_rownames)
 }
 
+
+#####################################################
+# prepare function to visualize using Seurat object #
+#####################################################
 
 spatial_feature_plot_seurat <- function(..., samples){
   plot_list <- list()
@@ -81,7 +82,7 @@ spatial_gene_plot_seurat <- function(data, gene, filt_score_int = 1000){
   
   # create plot for find peaks
   for(peak in vector_peak){
-    spatial_feature_plot(object = integrated_analysis,
+    spatial_feature_plot_seurat(object = integrated_analysis,
                          features = c(peak),
                          samples = samples_name) %>% 
       print()
@@ -89,108 +90,15 @@ spatial_gene_plot_seurat <- function(data, gene, filt_score_int = 1000){
 }
 
 
-spatial_gene_plot_seurat(data = sample_anno_all, gene = "Mag")
+spatial_gene_plot_seurat(data = spatial_transcriptomic_data$raw_data$annotate, gene = "Mag")
 
 
 
 
-spatial_feature_plot <- function(data,
-                                 data_cluster, 
-                                 peak_id,
-                                 samples,
-                                 barcode_data,
-                                 images_tibble,
-                                 min_percentile = 0.01,
-                                 max_percentile = 0.99,
-                                 size = 1.2,
-                                 normalization = TRUE){
-  
-  # normalize <- function(x) {
-  #   return((x - min(x)) / (max(x) - min(x)))
-  # }
-  
-  # prepare data to create plot
-  preprocessing_data <- data[peak_id, ] %>%
-    as.data.frame() %>%
-    rename(value = ".") %>% 
-    rownames_to_column("sample_barcode") %>%
-    separate("sample_barcode", c("sample", "barcode"), sep = "_") %>%
-    # left_join(., data_cluster, by = c("barcode", "sample")) %>%
-    left_join(., bcs_merge, by = c("barcode", "sample")) %>%
-    mutate(max_perc = as.numeric(quantile(value, probs = c(max_percentile))),
-           min_perc = as.numeric(quantile(value, probs = c(min_percentile))),
-           # trimming the extreme values to the adopted percentiles
-           value = ifelse(value < max_perc, value, max_perc),
-           value = ifelse(value > min_perc, value, min_perc),
-           # zero one normalization if normalization = TRUE
-           value = if (normalization == TRUE){
-             (value - min(value)) / (max(value) - min(value))
-           } else {
-             value
-           })
-
-  plot_list <- list()
-
-  # creating plot
-  for (i in 1:length(samples)){
-    sample_plot <- filter(preprocessing_data, sample == samples[i]) %>%
-      # main part of creating plot
-      {ggplot(., aes(x = imagecol, y = imagerow, fill = value)) +
-          geom_spatial(data = images_tibble[images_tibble$sample == samples[i],],
-                       aes(grob = grob),
-                       x = 0.5, y = 0.5) +
-          geom_point(shape = 21, colour = "black", size = size, stroke = 0.25) +
-          coord_cartesian(expand = FALSE) +
-          # chose correct scale fill gradientn during condition 
-          {if(normalization == TRUE){
-            scale_fill_gradientn(colours = myPalette(100), limits = c(0,1))
-          } else {
-            scale_fill_gradientn(colours = myPalette(100))
-          }} +
-          xlim(0, max(barcode_data %>%
-                        filter(sample == samples[i]) %>%
-                        select(width)))+
-          ylim(max(barcode_data %>%
-                     filter(sample == samples[i]) %>%
-                     select(height)), 0) +
-          # aesthetics plot
-          xlab("") +
-          ylab("") +
-          ggtitle(samples[i])+
-          theme_set(theme_bw(base_size = 10))+
-          theme(panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-                panel.background = element_blank(),
-                axis.line = element_line(colour = "black"),
-                axis.text = element_blank(),
-                axis.ticks = element_blank())}
-
-
-    plot_list[[samples[i]]] <- sample_plot
-
-  }
- 
-  wrap_plot <- wrap_plots(plot_list) +
-    plot_layout(ncol = 3, guides = "collect")
-
-  rm(plot_list)
-
-  return(wrap_plot)
-
-}
-
-
-
-spatial_feature_plot(data = colfilt_norm_data,
-                     data_cluster = data_cluster,
-                     samples = samples_name,
-                     peak_id = "merged-samples-peak-173070",
-                     barcode_data = bcs_merge,
-                     images_tibble = images_tibble,
-                     max_percentile = 0.99,
-                     min_percentile = 0.05,
-                     normalization = TRUE)
- 
+spatial_transcriptomic_data$raw_data$annotate %>%
+filter(gene_name == "Mag") %>%
+  filter(score_int..10.log10pvalue. > 1000) %>%
+  select(peak_id) %>% .[,1]
 
 
 
