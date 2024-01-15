e46bcbd2407b84e9d660b535aa7a26d68574761f
mateuszzieba <mateuszzieba97@gmail.com>
2024-01-15 12:55:31 +0000

    remove test-organization

--- preprocessing/shiny-app/test-organization-shiny-app/server.R
+++
@@ -1,9 +0,0 @@
-# Define server logic required to generate and plot a random distribution 
-server <- shinyServer(function(input, output) {
-    output$hist <- renderPlot({
-        # Generate a histogram with the requested number of observations
-        dist <- rnorm(input$slider)
-        hist(dist)
-    })
-})
-
